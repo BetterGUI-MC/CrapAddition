b8d05540b1edb9779cce0a099c7e6ba2204be6b2
HSGamer <huynhqtienvtag@gmail.com>
2020-02-12 19:14:43 +0700

    more work for TakeFromHandCommand

--- src/main/java/me/hsgamer/bettergui/crapaddition/command/TakeFromHandCommand.java
+++ src/main/java/me/hsgamer/bettergui/crapaddition/command/TakeFromHandCommand.java
@@ -1,7 +1,9 @@
 package me.hsgamer.bettergui.crapaddition.command;
 
+import java.util.Optional;
 import me.hsgamer.bettergui.crapaddition.Utils;
 import me.hsgamer.bettergui.lib.taskchain.TaskChain;
+import me.hsgamer.bettergui.object.ClickableItem;
 import me.hsgamer.bettergui.object.Command;
 import me.hsgamer.bettergui.util.Validate;
 import org.bukkit.entity.Player;
@@ -15,13 +17,21 @@ public class TakeFromHandCommand extends Command {
 
   @Override
   public void addToTaskChain(Player player, TaskChain<?> taskChain) {
-    String a = getParsedCommand(player);
-    if (Validate.isValidPositiveInteger(a)) {
+    String parsed = getParsedCommand(player);
+    int amount = 0;
+    Optional<ClickableItem> clickableItem =
+        getIcon().isPresent() ? getIcon().get().createClickableItem(player) : Optional.empty();
+    if (Validate.isValidPositiveInteger(parsed)) {
+      amount = Integer.parseInt(parsed);
+    } else if (parsed.equalsIgnoreCase("this") && clickableItem.isPresent()) {
+      amount = clickableItem.get().getItem().getAmount();
+    }
+    if (amount > 0) {
+      int finalAmount = amount;
       taskChain.sync(() -> {
-        int amount = Integer.parseInt(a);
         ItemStack item = Utils.getItem(player);
-        if (item.getAmount() > amount) {
-          item.setAmount(item.getAmount() - amount);
+        if (item.getAmount() > finalAmount) {
+          item.setAmount(item.getAmount() - finalAmount);
           Utils.setItemInHand(player, item);
         } else {
           Utils.setItemInHand(player, null);
