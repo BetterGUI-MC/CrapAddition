d8063d1ad640c2b8634a28b73f84c4c899688d2f
HSGamer <huynhqtienvtag@gmail.com>
2020-02-12 18:20:00 +0700

    forget to register

--- src/main/java/me/hsgamer/bettergui/crapaddition/Main.java
+++ src/main/java/me/hsgamer/bettergui/crapaddition/Main.java
@@ -3,6 +3,7 @@ package me.hsgamer.bettergui.crapaddition;
 import me.hsgamer.bettergui.builder.CommandBuilder;
 import me.hsgamer.bettergui.builder.IconBuilder;
 import me.hsgamer.bettergui.crapaddition.command.GiveThisCommand;
+import me.hsgamer.bettergui.crapaddition.command.TakeFromHandCommand;
 import me.hsgamer.bettergui.crapaddition.icon.HandIcon;
 import me.hsgamer.bettergui.object.addon.Addon;
 
@@ -12,5 +13,6 @@ public final class Main extends Addon {
   public void onEnable() {
     IconBuilder.register("hand", HandIcon.class);
     CommandBuilder.register("give-this", GiveThisCommand.class);
+    CommandBuilder.register("take-this-from-hand", TakeFromHandCommand.class);
   }
 }
